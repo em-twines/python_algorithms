faa1b3593ca8102e2ea97b911a47ef029a6605c0
emilytwines <emdelbridge@gmail.com>
2022-10-25 16:58:59 -0400

    Sheets 1 and 2 (inc Bonus) complete

--- problems_research.py
+++ problems_research.py
@@ -2,6 +2,7 @@
 # Task 2: use the random module to generate a rand number etween a range that includes favorite_number
     #Determine how many numbers away the random number was from your fav number.
 
+from operator import is_
 import random
 
 favorite_number = 24
@@ -38,8 +39,8 @@ while rand_num != favorite_number:
 print(f"It took the computer {counter} guesses to arrive at my favorite number, {favorite_number}.")
 
 
-
-#Task 4: Reverse a String
+#----------------------------------- pt 2------------------------------------------
+#Task 1: Reverse a String
     # steps: 
         # store string value
         # get last letter
@@ -69,4 +70,113 @@ reverse_string1("hello")
 
 
 
+# Task 2: Capitalize a Letter
+    # write code that takes a string as input and capitalizes the first letter of each word. Output as "Hello World".
+    # steps: 
+        # separate words into individual strings
+        # use string.captialize()
+        # print all strings together including their spaces
 
+def capitalization(str_phrase):
+    strs_phrase = str_phrase.split()
+    output = ""
+    for str in strs_phrase:
+        output += str.capitalize()
+        output += " "
+    print(output)
+
+capitalization("hello world")
+
+
+
+# Task 3: Palindrome
+    # Write code that takes user input and checks to see if it's a palindrome as well as reports the results. 
+    # steps
+        # define palindrome condition
+            # identify ver_string forwards
+            # identify ver_string backwards
+            # if forwards == backwards, then palendrome
+        # get input
+        # if input == palindrome
+        # print true phrase
+        # else print false phrase
+
+def palindrome(word):
+    is_palindrome = False
+    forward = word
+    backward = ""
+    for index in range(len(word) -1, -1, -1):
+        backward += word[index]
+    if forward == backward:
+        is_palindrome == True 
+        print(f"{word.capitalize()} is a palendrome!")
+    else:
+        print(f"{word.capitalize()} is not a palendrome.")
+
+palindrome("racecar")
+
+
+# Bonus Challenge
+# Task 4: compress repeated letters to a number then the letter they are the number of
+    # steps
+        # identify repeated letters
+        # compress to the number form of how many there are
+        # print 
+    
+def check_consecutive(string): 
+    output = []
+    count = 1
+    output1 = []
+    index = 1
+
+    for char in string:
+        while len(string) >= (index + 1):
+            char = string[index-1 ]
+            curr = string[index]
+            if char == curr:
+                count += 1 
+                index += 1
+                if index == len(string): 
+                    output1 += char   
+                    index += 1
+                    output.append(count)
+                    count = 1
+                    output1 += curr
+            else:
+                output1 += char   
+                index += 1
+                output.append(count)
+                count = 1
+
+    merged_output = merge_list(output, output1)
+    print(merged_output)
+
+def merge_list(list1, list2):
+    list_final = []
+    length_1 = len(list1)
+    length_2 = len(list2)
+    if length_1 >= length_2:
+        length = length_2
+        item = 0
+        while item < length:
+            list_final += str(list1[item]), list2[item]
+            item += 1
+        list_final_joined = "".join(list_final)
+        list_final_joined + (list2[item])
+
+    else: 
+        length = length_1
+        item = 0
+        while item < length:
+            list_final += str(list1[item]), list2[item]
+            item += 1
+        list_final_joined = "".join(list_final)
+        list_final_joined + (list2[item])
+    return list_final_joined
+
+
+check_consecutive("aaabbbbbccccaacccbbbaaabbbaaa")
+
+
+
+#----------------------------------- pt 3------------------------------------------
